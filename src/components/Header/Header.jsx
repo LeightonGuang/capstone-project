d883c7c54971a0fa720b5631bae2b1342f8c45ec
Leighton Guang <leighton.guang@gmail.com>
2023-11-06 15:57:46 +0000

    feat: add logo and tab icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import "./Header.scss";
+import logo from "../../assets/images/storedex_logo.svg";
 
 export default function Header() {
   return (
     <header className="header">
       <div className="header__logo-container">
-        <img src="" alt="logo" />
+        <img src={logo} alt="logo" className="header__logo" />
         <h1 className="header__title">Storedex</h1>
       </div>
       <div className="header__divider" />
