679820b1f7b70709c4bf19f04af4717e00222e40
Leighton Guang <leighton.guang@gmail.com>
2023-11-07 10:26:15 +0000

    feat: product card component

---
+++ src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,20 @@
+import "./ProductCard.scss";
+
+export default function ProductCard() {
+  return (
+    <li className="product-card">
+      <div className="product-card__container">
+        <img
+          src="https://www.price.com.hk/space/product/603000/603200_qeuyf7_0.jpg"
+          alt="product"
+          className="product-card__image"
+        />
+        <div className="product-card__divider" />
+        <div className="product-card__info">
+          <span>Intel 14900k</span>
+          <span>price $5000</span>
+        </div>
+      </div>
+    </li>
+  );
+}
