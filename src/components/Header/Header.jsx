6104c560bfe23278628a8703c92e76029e27cac2
Leighton Guang <leighton.guang@gmail.com>
2023-11-13 03:02:28 +0000

    feat: add search icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import "./Header.scss";
 import logo from "../../assets/images/storedex_logo.svg";
+import searchIcon from "../../assets/icons/search_FILL0_wght400_GRAD0_opsz24.svg";
 
 export default function Header() {
   return (
@@ -8,6 +9,16 @@ export default function Header() {
         <img src={logo} alt="logo" className="header__logo" />
         <h1 className="header__title">Storedex</h1>
       </div>
+      <form className="header__form">
+        <input type="text" className="header__search-input" />
+        <button type="submit" className="header__search-button">
+          <img
+            src={searchIcon}
+            alt="search icon"
+            className="header__search-icon"
+          />
+        </button>
+      </form>
       <div className="header__divider" />
     </header>
   );
