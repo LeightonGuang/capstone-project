19a9e6a854e7e46ba57d7c58b470f0bd04daedfc
Leighton Guang <leighton.guang@gmail.com>
2023-11-15 17:27:32 +0000

    fix: mobile menu spacing and icon

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,5 +1,9 @@
 import "./MobileMenu.scss";
 import { Link } from "react-router-dom";
+import homeIcon from "../../assets/icons/home-outline.svg";
+import categoryIcon from "../../assets/icons/category-outline.svg";
+import heartIcon from "../../assets/icons/heart-outline.svg";
+import profileIcon from "../../assets/icons/profile-outline.svg";
 
 export default function MobileMenu() {
   return (
@@ -8,26 +12,42 @@ export default function MobileMenu() {
         <ul className="mobile-menu__list">
           <Link to={"/"} className="mobile-menu__link">
             <li className="mobile-menu__img-container">
-              <img src="https://placehold.co/50x50" alt="Home Icon" />
-              Home
+              <img
+                src={homeIcon}
+                alt="Home Icon Created by Besticons from Noun Project"
+                className="mobile-menu__home-icon"
+              />
+              {/* Home */}
             </li>
           </Link>
           <Link to={"/category"} className="mobile-menu__link">
             <li className="mobile-menu__img-container">
-              <img src="https://placehold.co/50x50" alt="Category Icon" />
-              Category
+              <img
+                src={categoryIcon}
+                alt="Category Icon Created by Besticons from Noun Project"
+                className="mobile-menu__category-icon"
+              />
+              {/* Category */}
             </li>
           </Link>
           <Link to={"/saved"} className="mobile-menu__link">
             <li className="mobile-menu__img-container">
-              <img src="https://placehold.co/50x50" alt="Saved Icon" />
-              Saved
+              <img
+                src={heartIcon}
+                alt="Saved Icon Created by Besticons from Noun Project"
+                className="mobile-menu__heart-icon"
+              />
+              {/* Saved */}
             </li>
           </Link>
           <Link to={"/profile"} className="mobile-menu__link">
             <li className="mobile-menu__img-container">
-              <img src="https://placehold.co/50x50" alt="Profile Icon" />
-              Profile
+              <img
+                src={profileIcon}
+                alt="Profile Icon Created by Besticons from Noun Project"
+                className="mobile-menu__profile-icon"
+              />
+              {/* Profile */}
             </li>
           </Link>
         </ul>
