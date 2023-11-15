f7013651361731f2aefdbace736c4e259acad800
Leighton Guang <leighton.guang@gmail.com>
2023-11-15 15:28:34 +0000

    fix: simply the header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,21 @@
 import "./Header.scss";
 import logo from "../../assets/images/storedex_logo.svg";
 import searchIcon from "../../assets/icons/search_FILL0_wght400_GRAD0_opsz24.svg";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header__logo-container">
-        <img src={logo} alt="logo" className="header__logo" />
-        <h1 className="header__title">Storedex</h1>
-      </div>
-      <form className="header__form">
-        <input type="text" className="header__search-input" />
-        <button type="submit" className="header__search-button">
-          <img
-            src={searchIcon}
-            alt="search icon"
-            className="header__search-icon"
-          />
+      <div className="header__container">
+        <Link to={"/"} className="header__logo-container">
+          <img src={logo} alt="logo" className="header__logo" />
+          <h1 className="header__title">Storedex</h1>
+        </Link>
+
+        <button className="header__search-btn">
+          <img src={searchIcon} alt="" />
         </button>
-      </form>
-      <div className="header__divider" />
+      </div>
     </header>
   );
 }
