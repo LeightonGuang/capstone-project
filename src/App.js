667ddf59e685bea8ff6fe1cd05387c786f5033da
Leighton Guang <leighton.guang@gmail.com>
2023-11-16 00:12:21 +0000

    feat: create all the routes for mobile menu

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import HomePage from "./pages/HomePage/HomePage";
 import Header from "./components/Header/Header";
 import ProductPage from "./pages/ProductPage/ProductPage";
 import MobileMenu from "./components/MobileMenu/MobileMenu";
+import CategoryPage from "./pages/CategoryPage/CategoryPage";
+import FavouritePage from "./pages/FavouritePage/FavouritePage";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 export default function App() {
   return (
@@ -12,8 +15,10 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/category" element={<CategoryPage />} />
+          <Route path="/favourite" element={<FavouritePage />} />
+          <Route path="/profile" element={<ProfilePage />} />
           <Route path="/products/:id" element={<ProductPage />} />
-          
         </Routes>
         <MobileMenu />
       </BrowserRouter>
