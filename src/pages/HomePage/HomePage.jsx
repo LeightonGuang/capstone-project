ffc3392910fb31edff1ee8af1eb550a33bf48d51
Leighton Guang <leighton.guang@gmail.com>
2023-11-16 14:33:47 +0000

    feat: add hero card to the home screen

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,11 @@
 import DealOfTheWeek from "../../components/DealOfTheWeek/DealOfTheWeek";
+import HeroCard from "../../components/HeroCard/HeroCard";
 import "./HomePage.scss";
 export default function HomePage() {
   return (
     <main className="main">
       <div className="main__container">
+        <HeroCard />
         <DealOfTheWeek />
       </div>
     </main>
