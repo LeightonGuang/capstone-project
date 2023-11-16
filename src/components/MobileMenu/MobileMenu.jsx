e23cb396217227a25d4a0e8620edb3734244f2a7
Leighton Guang <leighton.guang@gmail.com>
2023-11-16 00:41:43 +0000

    fix: remove console.log

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -13,7 +13,6 @@ export default function MobileMenu() {
   const [isProfileActive, setIsProfileActive] = useState(false);
 
   const handleMenuClick = (event) => {
-    console.log(event.currentTarget.id);
     setIsHomeActive(false);
     setIsCategoryActive(false);
     setIsSavedActive(false);
