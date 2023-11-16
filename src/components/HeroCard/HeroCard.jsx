90e5ee1a3c3a361683bc324962ed2c2547aee74b
Leighton Guang <leighton.guang@gmail.com>
2023-11-16 14:33:29 +0000

    feat: general layout for hero card

---
+++ src/components/HeroCard/HeroCard.jsx
@@ -0,0 +1,15 @@
+import "./HeroCard.scss";
+import { Link } from "react-router-dom";
+
+export default function HeroCard() {
+  return (
+    <section className="hero">
+      <div className="hero__container">
+        <h2 className="hero__title">
+          Find the cheapest shop for what you want!
+        </h2>
+        <Link>Browse Now</Link>
+      </div>
+    </section>
+  );
+}
