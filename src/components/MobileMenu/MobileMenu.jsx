36c000b62504f9c6ce1655e85e8e43289564ddf3
Leighton Guang <leighton.guang@gmail.com>
2023-11-18 18:58:51 +0000

    refactor: remove useless code

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -4,39 +4,14 @@ import homeIcon from "../../assets/icons/home-outline.svg";
 import categoryIcon from "../../assets/icons/category-outline.svg";
 import heartIcon from "../../assets/icons/heart-outline.svg";
 import profileIcon from "../../assets/icons/profile-outline.svg";
-import { useState } from "react";
 
 export default function MobileMenu() {
-  const [isHomeActive, setIsHomeActive] = useState(true);
-  const [isCategoryActive, setIsCategoryActive] = useState(false);
-  const [isSavedActive, setIsSavedActive] = useState(false);
-  const [isProfileActive, setIsProfileActive] = useState(false);
-
-  const handleMenuHighlight = (event) => {
-    setIsHomeActive(false);
-    setIsCategoryActive(false);
-    setIsSavedActive(false);
-    setIsProfileActive(false);
-    if (event.currentTarget.id === "home-link") setIsHomeActive(true);
-    if (event.currentTarget.id === "category-link") setIsCategoryActive(true);
-    if (event.currentTarget.id === "saved-link") setIsSavedActive(true);
-    if (event.currentTarget.id === "profile-link") setIsProfileActive(true);
-  };
   return (
     <div className="mobile-menu">
       <nav className="mobile-menu__nav">
         <ul className="mobile-menu__list">
-          <NavLink
-            to={"/"}
-            className="mobile-menu__link"
-            id="home-link"
-            onClick={handleMenuHighlight}
-          >
-            <li
-              className={`mobile-menu__img-container ${
-                isHomeActive && "mobile-menu__img-container--selected"
-              }`}
-            >
+          <NavLink to={"/"} className="mobile-menu__link">
+            <li className="mobile-menu__img-container">
               <img
                 src={homeIcon}
                 alt="Home Icon Created by Besticons from Noun Project"
@@ -45,17 +20,8 @@ export default function MobileMenu() {
               {/* Home */}
             </li>
           </NavLink>
-          <NavLink
-            to={"/category"}
-            className="mobile-menu__link"
-            id="category-link"
-            // onClick={handleMenuHighlight}
-          >
-            <li
-              className={`mobile-menu__img-container ${
-                isCategoryActive && "mobile-menu__img-container--selected"
-              }`}
-            >
+          <NavLink to={"/category"} className="mobile-menu__link">
+            <li className="mobile-menu__img-container">
               <img
                 src={categoryIcon}
                 alt="Category Icon Created by Besticons from Noun Project"
@@ -65,17 +31,8 @@ export default function MobileMenu() {
             </li>
           </NavLink>
 
-          <NavLink
-            to={"/saved"}
-            className="mobile-menu__link"
-            id="saved-link"
-            onClick={handleMenuHighlight}
-          >
-            <li
-              className={`mobile-menu__img-container ${
-                isSavedActive && "mobile-menu__img-container--selected"
-              }`}
-            >
+          <NavLink to={"/saved"} className="mobile-menu__link">
+            <li className="mobile-menu__img-container">
               <img
                 src={heartIcon}
                 alt="Saved Icon Created by Besticons from Noun Project"
@@ -84,17 +41,8 @@ export default function MobileMenu() {
               {/* Saved */}
             </li>
           </NavLink>
-          <NavLink
-            to={"/profile"}
-            className="mobile-menu__link"
-            id="profile-link"
-            onClick={handleMenuHighlight}
-          >
-            <li
-              className={`mobile-menu__img-container ${
-                isProfileActive && "mobile-menu__img-container--selected"
-              }`}
-            >
+          <NavLink to={"/profile"} className="mobile-menu__link">
+            <li className="mobile-menu__img-container">
               <img
                 src={profileIcon}
                 alt="Profile Icon Created by Besticons from Noun Project"
