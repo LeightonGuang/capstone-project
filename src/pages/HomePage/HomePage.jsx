3ef6216f3dd4feeb4860f393173238f9cbe3326a
Leighton Guang <leighton.guang@gmail.com>
2023-11-20 22:47:56 +0000

    feat: home pages scrolls to top when it loads

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import DealOfTheWeek from "../../components/DealOfTheWeek/DealOfTheWeek";
 import HeroCard from "../../components/HeroCard/HeroCard";
 import "./HomePage.scss";
+import scrollToTop from "../../utils/scrollToTop";
+
 export default function HomePage() {
+  useEffect(() => {
+    scrollToTop();
+  });
+
   return (
     <main className="main">
       <div className="main__container">
