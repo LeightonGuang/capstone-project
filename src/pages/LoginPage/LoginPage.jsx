eb95b32839c97cb047dcd8bacb34ea01a1877031
Leighton Guang <leighton.guang@gmail.com>
2023-11-22 14:28:58 +0000

    feat: login returns a token from the api

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,5 +1,49 @@
 import "./LoginPage.scss";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Input from "../../components/Input/Input";
+import axios from "axios";
 
 export default function LoginPage() {
-  return;
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await axios.post(
+        `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/auth/login`,
+        {
+          email: event.target.email.value,
+          password: event.target.password.value,
+        }
+      );
+
+      sessionStorage.setItem("token", data.token);
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      setError(error.response.data);
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <article className="login__card">
+          <h2 className="login__title">Log in to your business</h2>
+
+          <form onSubmit={handleSubmit} className="login__form">
+            <Input type="email" name="email" label="Email" />
+            <Input type="password" name="password" label="Password" />
+
+            <button type="submit" className="login__button">
+              Log In
+            </button>
+          </form>
+        </article>
+      </div>
+    </div>
+  );
 }
