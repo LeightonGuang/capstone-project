1cbae6d124581fb0ae9208c14c318179c8243f02
Leighton Guang <leighton.guang@gmail.com>
2023-11-22 14:58:58 +0000

    feat: deals of the week selects 6 random products

--- src/components/DealOfTheWeek/DealOfTheWeek.jsx
+++ src/components/DealOfTheWeek/DealOfTheWeek.jsx
@@ -1,67 +1,61 @@
 import "./DealOfTheWeek.scss";
 import DealCard from "../DealCard/DealCard";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function DealOfTheWeek() {
+  const [deals, setDeals] = useState();
+
+  const getDeals = async () => {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/api/product`
+    );
+
+    const dealArray = [];
+    const indexs = [];
+
+    while (indexs.length < 6) {
+      const randomNumber = Math.floor(Math.random() * data.length);
+
+      if (indexs.indexOf(randomNumber) === -1) {
+        indexs.push(randomNumber);
+      }
+    }
+
+    indexs.forEach((i) => {
+      dealArray.push(data[i]);
+    });
+
+    setDeals(dealArray);
+  };
+
+  useEffect(() => {
+    getDeals();
+  }, []);
+
+  if (!deals) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section className="deal-of-the-week">
       <div className="deal-of-the-week__container">
         <h2 className="deal-of-the-week__subtitle">Deal of the week</h2>
         <div className="deal-of-the-week__list">
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/563000/563906_lztgc4_4.jpg"
-              }
-              productName={"Intel i5 13600k"}
-              price={"$200"}
-            />
-          </Link>
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/563000/563902_i6vyiq_4.jpg"
-              }
-              productName={"Intel i7 13700k"}
-              price={"$300"}
-            />
-          </Link>
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/563000/563899_kz5f5x_4.jpg"
-              }
-              productName={"Intel i9 13900k"}
-              price={"$600"}
-            />
-          </Link>
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/603000/603195_4zyg2i_4.jpg"
-              }
-              productName={"Intel i5 14600k"}
-              price={"$275"}
-            />
-          </Link>
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/603000/603197_buvjzj_4.png"
-              }
-              productName={"Intel i7 14700k"}
-              price={"$450"}
-            />
-          </Link>
-          <Link to={"/products/:id"} className="deal-of-the-week__link">
-            <DealCard
-              img={
-                "https://www.price.com.hk/space/product/603000/603200_qeuyf7_0.jpg"
-              }
-              productName={"Intel i9 14900k"}
-              price={"$700"}
-            />
-          </Link>
+          {deals.map((product) => (
+            <Link
+              to={`/products/${product.id}`}
+              key={product.id}
+              className="deal-of-the-week__link"
+            >
+              <DealCard
+                img={product.img_url}
+                productName={product.name}
+                price={"some price"}
+              />
+            </Link>
+          ))}
         </div>
       </div>
     </section>
