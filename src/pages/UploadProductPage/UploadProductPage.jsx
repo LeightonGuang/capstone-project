432d7de160f778e148f3693ed22c43bd933e2f40
Leighton Guang <leighton.guang@gmail.com>
2023-11-26 23:19:56 +0000

    feat: working upload listing

--- src/pages/UploadProductPage/UploadProductPage.jsx
+++ src/pages/UploadProductPage/UploadProductPage.jsx
@@ -2,24 +2,55 @@ import "./UploadProductPage.scss";
 import axios from "axios";
 import { useEffect, useState } from "react";
 import scrollToTop from "../../utils/scrollToTop";
+import Input from "../../components/Input/Input";
+import { useNavigate } from "react-router-dom";
 
 export default function UploadProductPage() {
   const [allProducts, setAllProducts] = useState();
+  const [allShops, setAllShops] = useState();
+  const navigate = useNavigate();
+
+  const handleUpload = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post(
+        `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/api/shop/listing`,
+        {
+          product_id: event.target.product.value,
+          shop_id: event.target.shop.value,
+          currency: event.target.currency.value,
+          price: event.target.price.value,
+        }
+      );
+
+      navigate(`/products/${event.target.product.value}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const getAllProducts = async () => {
     const { data } = await axios.get(
       `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/api/product`
     );
-    console.log(data);
     setAllProducts(data);
+  };
+
+  const getAllShops = async () => {
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/api/shop`
+    );
+    setAllShops(data);
   };
 
   useEffect(() => {
     scrollToTop();
     getAllProducts();
+    getAllShops();
   }, []);
 
-  if (!allProducts) {
+  if (!(allProducts && allShops)) {
     return <p>Loading..</p>;
   }
 
@@ -29,19 +60,26 @@ export default function UploadProductPage() {
         <article className="list-product__card">
           <h2 className="list-product__title">List Product</h2>
 
-          <form className="list-product__form">
+          <form className="list-product__form" onSubmit={handleUpload}>
             <label htmlFor="">Select a product</label>
-            <select name="" id="">
+            <select name="product">
               {allProducts.map((product) => (
                 <option key={product.id} value={product.id}>
                   {product.product_name}
                 </option>
               ))}
             </select>
-            <label htmlFor="">Currency</label>
-            <input type="text" />
-            <label htmlFor="">Price</label>
-            <input type="text" />
+            <label htmlFor="">Select a shop</label>
+            <select name="shop">
+              {allShops.map((shop) => (
+                <option key={shop.id} value={shop.id}>
+                  {shop.shop_name}
+                </option>
+              ))}
+            </select>
+            <Input type="text" name={"currency"} label={"Currency"} />
+            <Input type={"number"} name={"price"} label={"Price"} />
+            <input type="submit" value={"Upload"} />
           </form>
         </article>
       </div>
