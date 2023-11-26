45d52bd3e5c9be75e3211023dea3aaed77681489
Leighton Guang <leighton.guang@gmail.com>
2023-11-26 18:09:30 +0000

    feat: access to uploading listing when logged in

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,32 +1,76 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./ProfilePage.scss";
 import scrollToTop from "../../utils/scrollToTop";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function ProfilePage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [failedAuth, setFailedAuth] = useState(false);
+
+  const login = async () => {
+    const authToken = sessionStorage.getItem("authToken");
+    console.log(authToken);
+
+    try {
+      const { data } = await axios.get(
+        `${process.env.REACT_APP_API_URL}:${process.env.REACT_APP_PORT}/auth/profile`,
+        {
+          headers: {
+            Authorization: `Bearer ${authToken}`,
+          },
+        }
+      );
+
+      setFailedAuth(false);
+    } catch (error) {
+      setFailedAuth(true);
+    }
+    setIsLoading(false);
+  };
+
   useEffect(() => {
     scrollToTop();
+    login();
   }, []);
 
-  return (
-    <div className="profile">
-      <div className="profile__container">
-        <article className="profile__card">
-          <h2 className="profile__title">Profile</h2>
-          <div className="profile__btn-container">
-            <Link to={"/signup"} className="profile__setting-card">
-              Register as a business
-            </Link>
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
-            <Link to={"/login"} className="profile__setting-card">
-              Log In
-            </Link>
-            <Link to={"/uploadProduct"} className="profile__setting-card">
-              Upload a product listing
-            </Link>
-          </div>
-        </article>
+  if (!failedAuth) {
+    return (
+      <div className="profile">
+        <div className="profile__container">
+          <article className="profile__card">
+            <h2 className="profile__title">Profile</h2>
+            <div className="profile__btn-container">
+              <Link to={"/uploadProduct"} className="profile__setting-card">
+                Upload a product listing
+              </Link>
+            </div>
+          </article>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else if (failedAuth) {
+    return (
+      <div className="profile">
+        <div className="profile__container">
+          <article className="profile__card">
+            <h2 className="profile__title">Profile</h2>
+            <div className="profile__btn-container">
+              <Link to={"/signup"} className="profile__setting-card">
+                Register as a business
+              </Link>
+
+              <Link to={"/login"} className="profile__setting-card">
+                Log In
+              </Link>
+            </div>
+          </article>
+        </div>
+      </div>
+    );
+  }
 }
