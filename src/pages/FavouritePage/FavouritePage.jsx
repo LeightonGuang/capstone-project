7343dbc12e3a64e3444f38ec723a6f12fa6c0a25
Leighton Guang <leighton.guang@gmail.com>
2023-11-26 23:48:38 +0000

    fix: handle empty saved list

--- src/pages/FavouritePage/FavouritePage.jsx
+++ src/pages/FavouritePage/FavouritePage.jsx
@@ -37,37 +37,42 @@ export default function FavouritePage() {
       <div className="favourite__container">
         <article className="favourite__card">
           <ul className="favourite__list">
-            {favouriteList.map((listing, index) => {
-              return (
-                <li key={listing.saved_id} className="favourite-card">
-                  <Link
-                    to={`/products/${listing.product_id}`}
-                    className="favourite-card__link"
-                  >
-                    <img
-                      src={listing.product_img_url}
-                      alt={listing.product_name}
-                      className="favourite-card__img"
-                    />
-                    <div className="favourite-card__infos">
-                      <p className="favourite-card__name">
-                        {listing.product_name}
-                      </p>
-                      <span>{`${listing.currency} ${listing.price}`}</span>
-                    </div>
-                  </Link>
+            <h2 className="favourite__title">Saved</h2>
+            {favouriteList.length === 0 ? (
+              <div>You have no listing saved</div>
+            ) : (
+              favouriteList.map((listing) => {
+                return (
+                  <li key={listing.saved_id} className="favourite-card">
+                    <Link
+                      to={`/products/${listing.product_id}`}
+                      className="favourite-card__link"
+                    >
+                      <img
+                        src={listing.product_img_url}
+                        alt={listing.product_name}
+                        className="favourite-card__img"
+                      />
+                      <div className="favourite-card__infos">
+                        <p className="favourite-card__name">
+                          {listing.product_name}
+                        </p>
+                        <span>{`${listing.currency} ${listing.price}`}</span>
+                      </div>
+                    </Link>
 
-                  <img
-                    src={selectedHeartIcon}
-                    alt="save icon"
-                    onClick={() => {
-                      handleUnsaveClick(listing.saved_id);
-                    }}
-                    className="favourite-card__heart-icon"
-                  />
-                </li>
-              );
-            })}
+                    <img
+                      src={selectedHeartIcon}
+                      alt="save icon"
+                      onClick={() => {
+                        handleUnsaveClick(listing.saved_id);
+                      }}
+                      className="favourite-card__heart-icon"
+                    />
+                  </li>
+                );
+              })
+            )}
           </ul>
         </article>
       </div>
