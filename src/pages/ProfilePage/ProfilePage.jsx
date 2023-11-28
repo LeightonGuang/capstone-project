ed0bcb25a4532bd59be2ac335fe6e8c747764e4a
Leighton Guang <leighton.guang@gmail.com>
2023-11-28 23:50:48 +0000

    refactor: replace if else with tunary

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
 import "./ProfilePage.scss";
-import scrollToTop from "../../utils/scrollToTop";
-import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import scrollToTop from "../../utils/scrollToTop";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function ProfilePage() {
   const navigate = useNavigate();
@@ -47,47 +47,43 @@ export default function ProfilePage() {
     return <p>Loading...</p>;
   }
 
-  if (!failedAuth) {
-    return (
-      <div className="profile">
-        <div className="profile__container">
-          <article className="profile__card">
-            <h2 className="profile__title">{profile.shop_name}</h2>
-            <div className="profile__btn-container">
-              <Link to={"/uploadProduct"} className="profile__setting-btn">
-                Upload a product listing
-              </Link>
+  return failedAuth ? (
+    <div className="profile">
+      <div className="profile__container">
+        <article className="profile__card">
+          <h2 className="profile__title">Profile</h2>
+          <div className="profile__btn-container">
+            <Link to={"/signup"} className="profile__setting-btn">
+              Register as a business
+            </Link>
 
-              <Link to={"/manage-listing"} className="profile__setting-btn">
-                Manage Listing
-              </Link>
-
-              <p onClick={handleLogout} className="profile__setting-btn">
-                Log Out
-              </p>
-            </div>
-          </article>
-        </div>
+            <Link to={"/login"} className="profile__setting-btn">
+              Log In
+            </Link>
+          </div>
+        </article>
       </div>
-    );
-  } else if (failedAuth) {
-    return (
-      <div className="profile">
-        <div className="profile__container">
-          <article className="profile__card">
-            <h2 className="profile__title">Profile</h2>
-            <div className="profile__btn-container">
-              <Link to={"/signup"} className="profile__setting-btn">
-                Register as a business
-              </Link>
+    </div>
+  ) : (
+    <div className="profile">
+      <div className="profile__container">
+        <article className="profile__card">
+          <h2 className="profile__title">{profile.shop_name}</h2>
+          <div className="profile__btn-container">
+            <Link to={"/uploadProduct"} className="profile__setting-btn">
+              Upload a product listing
+            </Link>
 
-              <Link to={"/login"} className="profile__setting-btn">
-                Log In
-              </Link>
-            </div>
-          </article>
-        </div>
+            <Link to={"/manage-listing"} className="profile__setting-btn">
+              Manage Listing
+            </Link>
+
+            <p onClick={handleLogout} className="profile__setting-btn">
+              Log Out
+            </p>
+          </div>
+        </article>
       </div>
-    );
-  }
+    </div>
+  );
 }
