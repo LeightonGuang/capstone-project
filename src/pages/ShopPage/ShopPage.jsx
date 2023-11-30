7de0d4555940e4a5a69cb6bf26c2bbb222bbec9b
Leighton Guang <leighton.guang@gmail.com>
2023-11-30 11:47:22 +0000

    fix: styling for shop page

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -32,21 +32,31 @@ export default function ShopPage() {
     <div className="shop-page">
       <div className="shop-page__container">
         <article className="shop-page__card">
-          <img src={shopListing[0].shop_logo_url} alt="shop logo" />
-          <h2 className="shop-page__title">{shopListing[0].shop_name}</h2>
+          <div className="shop-page__shop-info-container">
+            <img
+              src={shopListing[0].shop_logo_url}
+              alt="shop logo"
+              className="shop-page__shop-logo"
+            />
+            <h2 className="shop-page__title">{shopListing[0].shop_name}</h2>
+          </div>
 
-          <ul className="shop-page__listing">
+          <ul className="shop-page__list">
             {shopListing.map((product) => {
               return (
                 <Link
                   to={`/products/${product.product_id}`}
                   key={product.listing_id}
+                  className="shop-page__shop-card"
                 >
                   <img
                     src={product.product_img_url}
                     alt={product.product_name}
+                    className="shop-page__shop-product-img"
                   />
-                  {product.product_name}
+                  <p className="shop-page__shop-product-title">
+                    {product.product_name}
+                  </p>
                 </Link>
               );
             })}
